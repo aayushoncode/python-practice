573bd47ce2a58f711f9ef481895498cd850bcad5
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-01 08:23:49 +0530

    day 3

--- assignment_lecture_3/revision.py
+++ assignment_lecture_3/revision.py
@@ -1,11 +1,30 @@
 # ask the user for string and check whether it is panlindrome or not.
 
-user = input(str("enter the string to check whether it is panlindrome or not : "))
-rev=""
-for i in range(len(user)-1,-1,-1):
-    rev+=user[i]
-check = user==rev
-if check == True:
-    print(f"yes {user} is a palindrome")
-else :
-    print(f"no {user} is not a palindrome")
+# user = input(str("enter the string to check whether it is panlindrome or not : "))
+# rev=""
+# for i in range(len(user)-1,-1,-1):
+#     rev+=user[i]
+# check = user==rev
+# if check == True:
+#     print(f"yes {user} is a palindrome")
+# else :
+#     print(f"no {user} is not a palindrome")
+
+
+
+
+# now by creating function
+
+def checkPalindrome(s):
+    if s == s[::-1]:
+        return "panlindrome" 
+    else :
+       return "not a palindrome"
+
+
+
+
+
+
+
+
