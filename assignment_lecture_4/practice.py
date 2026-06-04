e687df0608e00e105f3ba6bb1632e3a28fadfb1e
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-04 10:57:57 +0530

    day-3

---
+++ assignment_lecture_4/practice.py
@@ -0,0 +1,65 @@
+# learning classes and object 
+
+# class student ():
+#     def __init__(self,name,cgpa,subject,course): # parameterizes constructor
+#         self.name = name 
+#         self.cgpa=cgpa
+#         self.subject=subject
+#         self.course=course
+
+# stu1 = student("ayush",9.0,"python","bca")
+# print(f"{stu1.name} has {stu1.cgpa} cgpa \nsubject : {stu1.subject} \ncourse : {stu1.course}")
+
+
+# methods instance , class and static
+
+# class laptop :
+#     storage_type = "ssd"
+#     def __init__(self,RAM,STORAGE,GRACPHICS):
+#         self.RAM = RAM
+#         self.STORAGE = STORAGE
+#         self.GRACPHICS = GRACPHICS
+#     @classmethod
+#     def get_storage(cls):
+#         print(f"the storage_type of laptop is : {cls.storage_type}")
+#     def get_info(self):
+#         print(f"the laptop has ram = {self.RAM} storage {self.STORAGE} {self.storage_type} and graphics is {self.GRACPHICS}")
+#         return ""
+#     @staticmethod
+#     def calc_price(price,discount):
+#         final_price = price - (discount*price/100)
+#         print(f"the final price is {final_price}")
+        
+
+# lap1 = laptop("32gb","1tb","NVIDIA 4060")
+# # laptop.get_storage()
+
+# lap1.calc_price(50_000,10)
+
+
+
+
+
+
+# design and create an online store for product (name , price)
+#track total products being created 
+#create a stactic method to calculate discount on each product based on 
+# a % parameter 
+
+
+
+
+
+# class Rented_cars :
+#     def __init__(self,LdPRICE):
+#         self.BMW=40_00000
+#         self.BMW_PRICE=PRICE
+#     def get_info(self):
+#         print(f"{self.BMW} : {self.PRICE}")
+#     @staticmethod
+#     def calc_price(price):
+
+
+
+
+
